177f03b9ab55416c8918f1b03cab6dd55ccfd855
Vitalie <vitalie@toadi.com>
2022-07-09 13:19:07 +0200

    Added products

---
+++ components/ProductItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function ProductItem({ product }) {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`}>
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded-2xl shadow"
+          />
+        </a>
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5"></div>
+      <Link href={`/product/${product.slug}`}>
+        <a>
+          <h2 className="text-lg">{product.name}</h2>
+        </a>
+      </Link>
+      <p className="mb-2">{product.brand}</p>
+      <p>{product.price}</p>
+      <button className="primary-button" type="button">
+        Add To Cart
+      </button>
+    </div>
+  );
+}
